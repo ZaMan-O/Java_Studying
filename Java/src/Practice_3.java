e4cb7f4c58e45c0b56f0e3272b6d5221f6803e08
ZaMan <roblery128@gmail.com>
2025-03-25 20:18:59 +0900

    Java

--- Java/src/Practice_3.java
+++ Java/src/Practice_3.java
@@ -1,24 +1,24 @@
 public class Practice_3 {
     public static void main(String[] args) {
-    // if문 =========================
+        // if문 =========================
         int x = 10;
         int y = 15;
-        if(x < y) {
+        if (x < y) {
             System.out.println("x is smaller than y");
         }
 
-        if(x == y) {
+        if (x == y) {
             System.out.println("x is equal to y");
-        } else if(x > y){
+        } else if (x > y) {
             System.out.println("x is bigger than y");
         }
-    // 논리연산자 =========================
+        // 논리연산자 =========================
         boolean b1 = true;
         boolean b2 = false;
 
         System.out.println(b1 && b2);
         System.out.println(b1 || b2);
-    // 삼항연산자 =========================
+        // 삼항연산자 =========================
         int num = (5 > 4) ? 20 : 30;
         System.out.println(num);
     }
---
+++ Java/src/Practice_ETC.java
@@ -0,0 +1,7 @@
+public class Practice_ETC {
+    public static void main(String[] args) {
+        int num = 20;
+        int num2 = 50;
+        System.out.println(num + "" + num2);
+    }
+}
