8ecd68e10b9ea1eb62ee04031ca92b573e2f3250
ZaMan <roblery128@gmail.com>
2025-03-31 23:13:13 +0900

    Java

---
+++ Java/src/Practice_4.java
@@ -0,0 +1,20 @@
+public class Practice_4 {
+    public static void main(String[] args) {
+        // 비교연산자 =========================
+        int i = 10;
+        int j = 10;
+        System.out.println(i == j);
+        System.out.println(i != j);
+        System.out.println(i < j);
+        System.out.println(i <= j);// 비교 연산자의 결과는 boolean 형으로 나온다
+
+        i += 10; // i = i + 10
+        System.out.println(i);
+        System.out.println(i -= 5);
+        // 연산자 우선순위 =========================
+        int a = 5;
+        int b = 10;
+        int c = 15;
+        System.out.println(a + b * c);
+    }
+}
---
+++ Java/src/Practice_5.java
@@ -0,0 +1,5 @@
+public class Practice_5 {
+    public static void main(String[] args) {
+
+    }
+}
