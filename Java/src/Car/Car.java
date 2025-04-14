88ea0ea7a754ab7ef9878de69602379733e0618d
ZaMan <roblery128@gmail.com>
2025-04-14 15:47:34 +0900

    Java 4/13

---
+++ Java/src/Car/Car.java
@@ -0,0 +1,8 @@
+package Car;
+
+public class Car {
+    static String Car_name;
+    public Car(String str) {
+        Car_name = str;
+    }
+}
---
+++ Java/src/Car/CarClassTTest.java
@@ -0,0 +1,10 @@
+package Car;
+
+public class CarClassTTest {
+    public static void main(String[] args) {
+        Car c1 = new Car("소방차");
+        Car c2 = new Car("경찰차");
+        System.out.println(c1.Car_name);
+        System.out.println(c2.Car_name);
+    }
+}
